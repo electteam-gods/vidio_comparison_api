4970a83b1af24a58dd9093c4d0025697ae7d4ebb
OlegGayvoronsky <o.gajvoronskij03@gmail.com>
2024-09-28 01:38:41 +0300

    add_database
    add work with lancedb data base

--- video_comparision_api/app/main.py
+++ video_comparision_api/app/main.py
@@ -1,10 +1,15 @@
+import urllib
+import lancedb
 import requests
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
+import numpy as np
+import torch
+import torch.nn.functional as F
 import cv2
 from transformers import AutoImageProcessor, TimesformerForVideoClassification
 
+db = lancedb.connect("http://lancedb")
 model_name = "facebook/timesformer-base-finetuned-k400"
 processor = AutoImageProcessor.from_pretrained(model_name)
 model = TimesformerForVideoClassification.from_pretrained(model_name)
@@ -12,20 +17,20 @@ model = TimesformerForVideoClassification.from_pretrained(model_name)
 app = FastAPI()
 
 #функция для загрузки видео
-def load_video(video_path, num_frames=24, frame_height=480, frame_width=480):
+def load_video(video_path, frame_height=244, frame_width=244):
     cap = cv2.VideoCapture(video_path)
     frames = []
-    while len(frames) < num_frames:
-        ret, frame = cap.read()
-        if not ret:
-            break
+    k = 0
+    ret, frame = cap.read()
+    while ret:
+        k += 1
         frame = cv2.resize(frame, (frame_width, frame_height))
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frames.append(frame)
+        ret, frame = cap.read()
     cap.release()
-    if len(frames) < num_frames:
-        raise ValueError(f"Video is too short. Needed: {num_frames}, Found: {len(frames)}")
-    return frames
+    idxs = np.linspace(0, len(frames) - 1, 16, dtype=int)
+    return list(np.array(frames)[idxs])
 
 
 # Функция для получения предсказаний
@@ -38,18 +43,38 @@ def make_embedding(frames, model, processor):
 class VideoFile(BaseModel):
     url: str
 
+class Uuid(BaseModel):
+    id: str
+
 @app.post("/")
-async def process_video(video: VideoFile):
-    # считывание изображения
-    url = input.url
+async def process_video(uuid: Uuid, video: VideoFile):
+    if "emb_table" not in db.table_names():
+        tbl = db.create_table("emb_table", data=[])
+    else:
+        tbl = db.open_table("emb_table")
+    # считывание видео
+    result = {}
+    url = video.url
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            frames = load_video(response.content)
+            file_Path = 'short.mp4'
+            urllib.request.urlretrieve(response, file_Path)
+            emb = make_embedding(file_Path, model, processor)
+            numpy_emb = emb.cpu().detach().numpy()
+            res = tbl.search(numpy_emb).limit(1).metric("cosine").to_pandas()
+            vec = torch.tensor(list(res.vector)).squeeze(0)
+            cosine_similarity = F.cosine_similarity(emb, vec, dim=0).item()
+            if cosine_similarity < 0.48:
+                data = [{"vector": numpy_emb, "id": uuid}]
+                tbl.add(data)
+                result['answer'] = False
+                result['id'] = ""
+            else:
+                result['answer'] = True
+                result['id'] = res.id
         else:
             HTTPException(status_code=404, detail="Failed to download image")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-    emb = make_embedding(frames, model, processor)
-    return {"vector": emb}
+    return result
